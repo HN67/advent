5a2464145ac189396989913259087f723fe3b06c
HN67 <hn67allardkids@gmail.com>
2021-12-02 22:49:33 -0700

    Add logging configurer to core

--- advent/year2021/core.py
+++ advent/year2021/core.py
@@ -1,7 +1,36 @@
 """Core utilities for Advent of Code 2021."""
 
 import argparse
+import logging
 import typing as t
+
+
+# Adapted from HN67/nsapi
+def configure_logger(
+    loggerObject: logging.Logger,
+    *,
+    level: t.Union[int, str] = logging.WARNING,
+    format_string: str = "[%(asctime)s] [%(levelname)s] %(name)s - %(message)s",
+    force: bool = True,
+) -> logging.Logger:
+    """Performs standard configuration on the provided logger.
+
+    Can be used to configure this modules logger or any user modules logger.
+
+    Adds a default stream handler with a format string containing level, name, and message.
+
+    Returns the logger passed.
+    """
+    # Add formatted handler
+    # Only add the handler if forced or none exist
+    if force or len(loggerObject.handlers) == 0:
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter(format_string))
+        loggerObject.addHandler(handler)
+    # Set logging level
+    loggerObject.setLevel(level)
+    # Chain object
+    return loggerObject
 
 
 def cmd(day: int, one: t.Callable[[], None], two: t.Callable[[], None]) -> None:
