48e804ffe6b0a13a77088a9b1ecab1511c9f6b39
HN67 <hn67allardkids@gmail.com>
2021-12-02 22:36:29 -0700

    Attempt to solve part 2 of day 2

--- advent/year2021/day2.py
+++ advent/year2021/day2.py
@@ -1,5 +1,6 @@
 """Solution to Day 2 of AoC."""
 
+import abc
 import collections.abc as c
 import dataclasses
 import functools
@@ -8,7 +9,18 @@ import typing as t
 
 from . import core
 
-S = t.TypeVar("S", bound="Position")
+SU = t.TypeVar("SU", bound="Updatable")
+
+
+class Updatable(t.Protocol):
+    """Type that can respond to commands."""
+
+    @abc.abstractmethod
+    def update(self: SU, action: str, value: int) -> SU:
+        """Create the result of a command."""
+
+
+SP = t.TypeVar("SP", bound="Position")
 
 
 @dataclasses.dataclass()
@@ -18,37 +30,66 @@ class Position:
     horizontal: int
     depth: int
 
-    def __add__(self: S, other: "Position") -> S:
+    def __add__(self: SP, other: "Position") -> SP:
         """Element-wise sum two positions."""
         return type(self)(
             horizontal=self.horizontal + other.horizontal,
             depth=self.depth + other.depth,
         )
 
-    def move(self, action: str, distance: int) -> "Position":
+    def update(self: SP, action: str, value: int) -> SP:
         """Create the position resulting from a move from this position."""
         match action:
             case "forward":
-                return self + Position(horizontal=distance, depth=0)
+                return self + Position(horizontal=value, depth=0)
             case "down":
-                return self + Position(horizontal=0, depth=distance)
+                return self + Position(horizontal=0, depth=value)
             case "up":
-                return self + Position(horizontal=0, depth=-distance)
+                return self + Position(horizontal=0, depth=-value)
             case _:
                 raise ValueError(f"Action '{action}' is not recognized.")
 
 
-def apply_movement(initial: Position, delta: tuple[str, int]) -> Position:
-    """Functionally apply a delta onto a Position."""
-    return initial.move(delta[0], delta[1])
+SS = t.TypeVar("SS", bound="State")
 
 
-def chart(
-    commands: c.Iterable[tuple[str, int]], initial: t.Optional[Position] = None
-) -> Position:
+@dataclasses.dataclass()
+class State:
+    """State of a submarine, position and aim."""
+
+    position: Position
+    aim: int
+
+    def __add__(self: SS, other: "State") -> SS:
+        """Element-wise sum two states."""
+        return type(self)(
+            position=self.position + other.position, aim=self.aim + other.aim
+        )
+
+    def update(self: SS, action: str, value: int) -> SS:
+        """Create the position resulting from a move from this position."""
+        match action:
+            case "forward":
+                return self + State(
+                    position=self.position
+                    + Position(horizontal=value, depth=self.aim * value),
+                    aim=0,
+                )
+            case "down":
+                return self + State(position=Position(0, 0), aim=value)
+            case "up":
+                return self + State(position=Position(0, 0), aim=-value)
+            case _:
+                raise ValueError(f"Action '{action}' is not recognized.")
+
+
+def apply_movement(initial: SU, delta: tuple[str, int]) -> SU:
+    """Functionally apply a delta onto a Updatable."""
+    return initial.update(delta[0], delta[1])
+
+
+def chart(commands: c.Iterable[tuple[str, int]], initial: SU) -> SU:
     """Chart the resulting position based on provided commands."""
-    if initial is None:
-        initial = Position(0, 0)
     return functools.reduce(apply_movement, commands, initial)
 
 
@@ -57,20 +98,26 @@ def parse_components(pieces: c.Sequence[str]) -> tuple[str, int]:
     return (pieces[0], int(pieces[1]))
 
 
-def part_one() -> None:
-    """Solve part one of day 2."""
-    commands: t.Iterable[tuple[str, int]] = (
-        parse_components(line.strip().split(" ")) for line in sys.stdin
-    )
-    destination = chart(commands)
+def load_commands() -> t.Iterable[tuple[str, int]]:
+    """Load commands from standard input."""
+    return (parse_components(line.strip().split(" ")) for line in sys.stdin)
+
+
+def display_result(destination: Position) -> None:
+    """Format display the resulting destination."""
     print(f"Destination: {destination}")
     product = destination.horizontal * destination.depth
     print(f"Product: {product}")
 
 
+def part_one() -> None:
+    """Solve part one of day 2."""
+    display_result(chart(load_commands(), initial=Position(0, 0)))
+
+
 def part_two() -> None:
     """Solve part two of day 2."""
-    return apply_movement(3, "2")
+    display_result(chart(load_commands(), initial=State(Position(0, 0), 0)).position)
 
 
 if __name__ == "__main__":
