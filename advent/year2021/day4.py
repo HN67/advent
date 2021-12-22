7c540d0f17f25c93fc27f636223a00afee856aa0
HN67 <hn67allardkids@gmail.com>
2021-12-21 21:36:47 -0700

    Solve day 4 part 1

--- advent/year2021/day4.py
+++ advent/year2021/day4.py
@@ -2,7 +2,9 @@
 
 import collections.abc as c
 import dataclasses
+import itertools
 import logging
+import sys
 import typing as t
 
 from . import core
@@ -18,7 +20,7 @@ class Tile(t.Generic[TCo]):
     """Tile of a board."""
 
     value: TCo
-    marked: bool
+    marked: bool = False
 
 
 BoardS = t.TypeVar("BoardS", bound="Board")
@@ -55,11 +57,68 @@ class Board(t.Generic[T]):
 
     def unmarked(self) -> c.Iterable[T]:
         """The values of unmarked tiles."""
-        return (tile.value for row in self.rows for tile in row)
+        return (tile.value for row in self.rows for tile in row if not tile.marked)
+
+
+def read_input() -> tuple[c.Iterable[int], c.Iterable[Board[int]]]:
+    """Read the puzzle input."""
+
+    # Get the called numbers
+    calls = [int(number) for number in input().split(",")]
+
+    # Advance stdin
+    input()
+
+    boards = [
+        Board(
+            # Whitespace split() consumes multiple spaces
+            [[Tile(int(number)) for number in row.strip().split()] for row in group]
+        )
+        for key, group in itertools.groupby(sys.stdin, key=lambda line: line == "\n")
+        if not key
+    ]
+
+    return calls, boards
+
+
+def winner(
+    calls: c.Iterable[T], boards: c.Iterable[Board[T]]
+) -> t.Optional[tuple[T, Board[T]]]:
+    """Find the first winning board.
+
+    Iterates calls until a winning board is found,
+    returning it.
+
+    Returns the first board in the iterable if multiple win simultaneously.
+
+    Returns None if no boards win after calls is exhausted.
+    """
+    for call in calls:
+        # Mark the call
+        boards = [board.mark(call) for board in boards]
+        # Check for a winner
+        for board in boards:
+            if board.complete():
+                return call, board
+    return None
+
+
+def valuate(call: int, board: Board[int]) -> int:
+    """The value of a completed board with the winning call."""
+    return sum(board.unmarked()) * call
 
 
 def part_one() -> None:
     """Solve Part One."""
+    calls, boards = read_input()
+    winning = winner(calls, boards)
+    if winning is None:
+        print("No winner.")
+    else:
+        call, board = winning
+        print(f"Call: {call}")
+        print(f"Board: {board}")
+        print(f"Score: {valuate(call, board)}")
 
 
 def part_two() -> None:
