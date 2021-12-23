d46a434e0bd701c8e0888576ded3a15f4062d017
HN67 <hn67allardkids@gmail.com>
2021-12-22 21:28:19 -0700

    Create some basic dataclasses

--- advent/year2021/day5.py
+++ advent/year2021/day5.py
@@ -1,12 +1,33 @@
 """Solution for Day 5 of AoC."""
 
+import dataclasses
 import logging
+import typing as t
 
 from . import core
 
 DAY = 5
 
+T = t.TypeVar("T")
+TCo = t.TypeVar("TCo", covariant=True)
+
 logger = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass(frozen=True)
+class Point(t.Generic[TCo]):
+    """2D Point."""
+
+    x: TCo
+    y: TCo
+
+
+@dataclasses.dataclass(frozen=True)
+class Box(t.Generic[TCo]):
+    """Euclidean 2D Box."""
+
+    origin: Point[TCo]
+    corner: Point[TCo]
 
 
 def part_one() -> None:
