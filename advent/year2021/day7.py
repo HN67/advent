aec4ec28ea4f549ff04b08e51f8bc1ea2dac2648
HN67 <hn67allardkids@gmail.com>
2021-12-31 23:53:46 -0700

    Create basic Day 7 structure

---
+++ advent/year2021/day7.py
@@ -0,0 +1,24 @@
+"""Solution for Day 7 of AoC."""
+
+import collections.abc as c
+import logging
+import typing as t
+
+from . import core
+
+logger = logging.getLogger(__name__)
+
+DAY = 7
+
+
+def part_one() -> None:
+    """Solve Part One"""
+
+
+def part_two() -> None:
+    """Solve Part Two."""
+
+
+if __name__ == "__main__":
+    core.configure_logger(logger, level=logging.INFO)
+    core.cmd(DAY, part_one, part_two)
