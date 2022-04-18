a7f2e32bb6a039f666ce59589e980a14cf5e2f7c
HN67 <hn67allardkids@gmail.com>
2022-04-18 13:36:00 -0600

    Day 7 Part 1

--- advent/year2021/day7.py
+++ advent/year2021/day7.py
@@ -2,7 +2,8 @@
 
 import collections.abc as c
 import logging
-import typing as t
+import statistics
+import sys
 
 from . import core
 
@@ -11,8 +12,27 @@ logger = logging.getLogger(__name__)
 DAY = 7
 
 
+def alignment_cost(positions: c.Iterable[int], target: int = 0) -> int:
+    """The total cost (distance) to align each position to the target."""
+    return sum(abs(pos - target) for pos in positions)
+
+
+def optimal_target(positions: c.Iterable[int]) -> int:
+    """The optimal alignment target."""
+    return statistics.median_low(positions)
+
+
+# [1, 0, 5] counter examples arithmetic mean
+# [1, 3, 5] and [0, 1, 5] both have median
+
+
 def part_one() -> None:
     """Solve Part One"""
+    # need a list comprehension since we need to iterate over it twice
+    positions = [
+        int(raw) for line in core.load_data(sys.stdin) for raw in line.split(",")
+    ]
+    print(alignment_cost(positions, optimal_target(positions)))
 
 
 def part_two() -> None:
