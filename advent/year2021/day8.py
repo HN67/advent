8e6ebcb4567329b6bbf54e4a06d8e079324ec09d
HN67 <hn67allardkids@gmail.com>
2022-04-21 14:47:06 -0600

    Day 8 structure

---
+++ advent/year2021/day8.py
@@ -0,0 +1,17 @@
+"""Solution for Day 8 of AoC."""
+
+from . import core
+
+
+def part_one() -> None:
+    """Solution for Part One."""
+
+
+def part_two() -> None:
+    """Solution for Part Two."""
+
+
+DAY = 8
+
+if __name__ == "__main__":
+    core.cmd(DAY, part_one, part_two)
