fc73f7c272a3619e3aee5f769e8b5faad5345c49
HN67 <hn67allardkids@gmail.com>
2022-12-14 09:37:37 -0700

    Add logging structure

--- advent/year2022/__main__.py
+++ advent/year2022/__main__.py
@@ -1,14 +1,20 @@
 """Main entrypoint for Advent of Code 2022 solutions."""
 
+import logging
 import sys
 
 from . import core
 from . import day1
 
+logger = logging.getLogger(__name__)
+
 
 # Actual entrypoint
 def main() -> None:
     """Main entrypoint."""
+    root_logger = logging.getLogger()
+    core.configure_logger(root_logger)
+
     runner = core.Runner()
     for day in (day1,):
         runner.load_component(day.component)
--- advent/year2022/day1.py
+++ advent/year2022/day1.py
@@ -1,5 +1,17 @@
 """Solution to Day 1 of AoC."""
 
+import logging
+import typing as t
+
 from . import core
 
+logger = logging.getLogger(__name__)
+
+# Define component to be imported by main
 component = core.Component()
+
+
+@component.hook(1, 1)
+def one(input_stream: t.TextIO, output_stream: t.TextIO) -> None:
+    """Day 1 Part 1 solution."""
+    logger.warning("Not implemented")
