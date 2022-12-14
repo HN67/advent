0a9ca134866f531faa21897f8763405d1a7d6f92
HN67 <hn67allardkids@gmail.com>
2022-12-14 10:43:58 -0700

    Solve Day 1 part 2

--- advent/year2022/day1.py
+++ advent/year2022/day1.py
@@ -29,3 +29,16 @@ def one(input_stream: t.TextIO, output_stream: t.TextIO) -> None:
     # Real data will never be empty but for max to type correctly we should provide a default
     most: int = max(map(sum, elves), default=0)
     print(f"Most total calories: {most}", file=output_stream)
+
+
+@component.hook(1, 2)
+def two(input_stream: t.TextIO, output_stream: t.TextIO) -> None:
+    """Day 1 Part 2 solution."""
+    lines = core.load_data(input_stream)
+    elves = parse_data(lines)
+    # Sort the elves carrying calories by highest first
+    carries = sorted([sum(elf) for elf in elves], reverse=True)
+    # Take the top 3
+    top = carries[:3]
+    total = sum(top)
+    print(f"Top three total calories: {total}", file=output_stream)
