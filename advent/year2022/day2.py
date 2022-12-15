98a87c9d91c2df2efde2008bd8cdda3e3ad18578
HN67 <hn67allardkids@gmail.com>
2022-12-15 13:09:21 -0700

    Solve Day 2 Part 2

--- advent/year2022/day2.py
+++ advent/year2022/day2.py
@@ -26,8 +26,30 @@ class RPS(enum.Enum):
 
     @classmethod
     def from_player(cls, player: str) -> "RPS":
-        """Parse an player code."""
+        """Parse n player code."""
         return {"X": cls.ROCK, "Y": cls.PAPER, "Z": cls.SCISSORS}[player]
+
+    @classmethod
+    def from_player_goal(cls, player: str, opponent: "RPS") -> "RPS":
+        """Parse a player goal based on specific opponent."""
+        # Lose
+        if player == "X":
+            return {
+                RPS.ROCK: cls.SCISSORS,
+                RPS.PAPER: cls.ROCK,
+                RPS.SCISSORS: cls.PAPER,
+            }[opponent]
+        # Tie
+        if player == "Y":
+            return opponent
+        # Win
+        if player == "Z":
+            return {
+                RPS.ROCK: cls.PAPER,
+                RPS.PAPER: cls.SCISSORS,
+                RPS.SCISSORS: cls.ROCK,
+            }[opponent]
+        raise ValueError(f"Invalid player code '{player}'")
 
 
 # Winning plays
@@ -62,9 +84,26 @@ def parse_game(encoding: str) -> t.Tuple[RPS, RPS]:
     return (RPS.from_player(player), RPS.from_opponent(opponent))
 
 
+def parse_desired_game(encoding: str) -> t.Tuple[RPS, RPS]:
+    """Parse a single round specification, where the second value is desired outcome."""
+    opponent, player = encoding.split()
+    opponent_symbol = RPS.from_opponent(opponent)
+    player_symbol = RPS.from_player_goal(player, opponent_symbol)
+    logger.info("O/P: %s %s", opponent_symbol, player_symbol)
+    return (player_symbol, opponent_symbol)
+
+
 @component.hook(2, 1)
 def one(input_stream: t.TextIO, output_stream: t.TextIO) -> None:
     """Solve Day 2 Part 1."""
     games = [parse_game(line) for line in core.load_data(input_stream)]
+    total = sum(score(player, opponent) for player, opponent in games)
+    print(f"Total score: {total}", file=output_stream)
+
+
+@component.hook(2, 2)
+def two(input_stream: t.TextIO, output_stream: t.TextIO) -> None:
+    """Solve Day 2 Part 2."""
+    games = [parse_desired_game(line) for line in core.load_data(input_stream)]
     total = sum(score(player, opponent) for player, opponent in games)
     print(f"Total score: {total}", file=output_stream)
